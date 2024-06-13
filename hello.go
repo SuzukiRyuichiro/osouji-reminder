9913ded1b90c890927cfc5f959cc61bfe9fd63ee
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-06-13 15:40:53 +0900

    trying some struct stuff

--- hello.go
+++ hello.go
@@ -2,7 +2,33 @@ package main
 
 import "fmt"
 
+type Human struct {
+	Name   string
+	Age    int
+	Weight float32
+}
+
 func main() {
-	name := "Scooter"
-	fmt.Println("hello world" + " " + name)
+	scooter := Human{
+		Name:   "Ryuichiro Suzuki",
+		Age:    24,
+		Weight: 60.3,
+	}
+	message := fmt.Sprintf("hi %s, you are %d years old and weigh %.1f kg", scooter.Name, scooter.Age, scooter.Weight)
+	fmt.Println(message)
+	fmt.Println(addOne(100))
+
+	scooter.addWeight(4.4)
+
+	message = fmt.Sprintf("hi %s, you are %d years old and weigh %.1f kg", scooter.Name, scooter.Age, scooter.Weight)
+	fmt.Println(message)
+
+}
+
+func addOne(i int) int {
+	return i + 1
+}
+
+func (h *Human) addWeight(f float32) {
+	h.Weight += f
 }
