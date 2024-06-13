a0f6c0f30623800e759c51ff07f80a99ec5c45ac
Ryuichiro Suzuki <dragon.aka.scooter@gmail.com>
2024-06-13 14:57:39 +0900

    Initial commit

---
+++ test/osouji-reminder.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OsoujiReminder from '../lib/osouji-reminder-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/osouji-reminder-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OsoujiReminder.OsoujiReminderStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
